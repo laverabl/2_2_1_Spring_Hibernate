296c0cbbbc18bd48d45f32ed6b9a4b1ddb2ae15c
laverabl <podoprygina@MacBook-Air-Valeria.local>
2023-09-28 17:15:04 +0300

    add User.class

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -26,7 +26,7 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User", User.class);
       return query.getResultList();
    }
    @Override
